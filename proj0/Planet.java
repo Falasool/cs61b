2f829a2c9c853e0c48594a66daaa004715a193d0
Falasool <meetyou202020@gmailcom>
2024-08-01 13:25:23 +0800

    create Planet and define basic vars

---
+++ proj0/Planet.java
@@ -0,0 +1,14 @@
+public class Planet {
+    //    current x position
+    double xxPos;
+    // current y postion
+    double yyPos;
+    //    current velocity in x direction
+    double xxVel;
+    // current vleocity in y direction
+    double yyVel;
+    // mass 质量
+    double mass;
+    // 行星图片的文件名 planet
+    String imgFileName;
+}
